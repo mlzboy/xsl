1e38921def79f3c82685c4887ffa5143dcd9eb20
John Doe <john@sample.com>
2020-09-06 11:30:19 +0800

    朋友圈增加产品介绍分享功能，换图片
    rt

--- pages/video.js
+++ pages/video.js
@@ -21,7 +21,13 @@ Page({
   onReady: function () {
 
   },
-
+  onShareTimeline: function(res){
+    return {
+      title: '教师课表小程序功能介绍',
+      query: '/pages/video',
+      imageUrl: "/static/kcb.png"//自定义图片路径，可以是本地文件路径、代码包文件路径或者网络图片路径。支持PNG及JPG。显示图片长宽比是 5:4。
+    }
+  },
   /**
    * 生命周期函数--监听页面显示
    */
@@ -61,7 +67,23 @@ Page({
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function (ops) {
+    if (ops.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(ops.target)
+    }
+    return {
+      title: "教师课表小程序功能介绍",
+      path: 'pages/video',
+      success: function (res) {
+        // 转发成功
+        console.log("转发成功:" + JSON.stringify(res));
+      },
+      fail: function (res) {
+        // 转发失败
+        console.log("转发失败:" + JSON.stringify(res));
+      }
+    }
 
   }
 })
